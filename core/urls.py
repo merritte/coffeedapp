7799a135fdfde328935b1de8000f3dfb5ac87b1b
Shay-Jahen Merritté <shay@merritte.com>
2015-04-19 23:37:15 +0000

    adding images to locations

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 
  url(r'^$', coreviews.LandingView.as_view()),
  url(r'locations/$', coreviews.LocationListView.as_view()),
+ url(r'locations/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='locations_list'),
 )
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 import core.models as coremodels
 
 # Create your views here.
@@ -8,10 +9,11 @@ import core.models as coremodels
 class LandingView(TemplateView):
     template_name = "base/index.html"
 
-#class LocationListView(ListView):
-    #model = coremodels.Location
-    #template_name = 'location/list.html'
-    
 class LocationListView(ListView):
     model = coremodels.Location
-    template_name = 'base/theme.html'
+    template_name = 'location/list.html'
+
+class LocationDetailView(DetailView):
+    model = coremodels.Location
+    template_name = 'location/detail.html'
+    context_object_name = 'location'
