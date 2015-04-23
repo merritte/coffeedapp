9d00288dc172ce8aa7e804fef611488b954e9e86
Shay-Jahen Merritté <shay@merritte.com>
2015-04-23 02:13:54 +0000

    added search and edit functionality

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,8 @@ urlpatterns = patterns('',
  url(r'^$', coreviews.LandingView.as_view()),
  url(r'locations/$', coreviews.LocationListView.as_view()),
  url(r'locations/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='locations_list'),
- url(r'location/create/$', coreviews.LocationCreateView.as_view()), 
+ url(r'location/create/$', coreviews.LocationCreateView.as_view()),
+ url(r'search/$', coreviews.SearchListView.as_view()),
+url(r'locations/(?P<pk>\d+)/update/$', coreviews.LocationUpdateView.as_view(), name='location_update'),
+ 
 )
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 import core.models as coremodels
 
 # Create your views here.
@@ -13,6 +13,11 @@ class LandingView(TemplateView):
 class LocationListView(ListView):
     model = coremodels.Location
     template_name = 'location/list.html'
+    
+class SearchListView(LocationListView):
+    def get_queryset(self):
+        incoming_query_string = self.request.GET.get('query', '')
+        return coremodels.Location.objects.filter(title__icontains=incoming_query_string)
 
 class LocationDetailView(DetailView):
     model = coremodels.Location
@@ -23,3 +28,8 @@ class LocationCreateView(CreateView):
   model = coremodels.Location
   template_name = 'base/form.html'
   fields = "__all__"
+
+class LocationUpdateView(UpdateView):
+    model = coremodels.Location
+    template_name = 'base/form.html'
+    fields = "__all__"
