14ae909bf68b21c706c0d19a86182b87109d2606
Shay-Jahen Merritté <shay@merritte.com>
2015-04-25 01:15:59 +0000

    added pagination

--- core/views.py
+++ core/views.py
@@ -14,6 +14,7 @@ class LandingView(TemplateView):
 class LocationListView(ListView):
     model = coremodels.Location
     template_name = 'location/list.html'
+    paginate_by = 5
     
 class SearchListView(LocationListView):
     def get_queryset(self):
