44d3954250feab08e0c186b7981b03c0b8339c16
Shay-Jahen Merritté <shay@merritte.com>
2015-04-29 16:41:31 +0000

    added homepage link to locations

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import core.views as coreviews
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
 
  url(r'^$', coreviews.LandingView.as_view()),
  url(r'locations/$', coreviews.LocationListView.as_view()),
  url(r'locations/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='locations_list'),
- url(r'locations/create/$', coreviews.LocationCreateView.as_view()),
+ url(r'locations/create/$', login_required(coreviews.LocationCreateView.as_view())),
  url(r'search/$', coreviews.SearchListView.as_view()),
- url(r'locations/(?P<pk>\d+)/update/$', coreviews.LocationUpdateView.as_view(), name='location_update'),
- url(r'locations/(?P<pk>\d+)/review/create/$', coreviews.ReviewCreateView.as_view(), name='review_create'),
- url(r'locations/(?P<pk>\d+)/review/update/$', coreviews.ReviewUpdateView.as_view(), name='review_update'),
+ url(r'locations/(?P<pk>\d+)/update/$', login_required(coreviews.LocationUpdateView.as_view()), name='location_update'),
+ url(r'locations/(?P<pk>\d+)/review/create/$', login_required(coreviews.ReviewCreateView.as_view()), name='review_create'),
+ url(r'locations/(?P<pk>\d+)/review/update/$', login_required(coreviews.ReviewUpdateView.as_view()), name='review_update'),
  url(r'entrance/$', coreviews.entrance),
 )
